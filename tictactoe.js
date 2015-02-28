61e44e7dd23dbe7477a4440f43d6c7c47182f1be
Rich Churcher <rich.churcher@gmail.com>
2015-02-28 18:32:00 +1300

    Click on square produces tick.

--- tictactoe.js
+++ tictactoe.js
@@ -1,2 +1,25 @@
 // Vanilla JavaScript challenge: write Tic Tac Toe without consulting a reference 
 // (other than MDN for syntax.)
+var makeMove = function(e) {
+    e.toElement.classList.add("o");
+}
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+/*    document.getElementById("tictactoe").addEventListener("click", function () {
+        document.body.classList.toggle("left-active");
+        document.body.classList.remove("top-active", "right-active");
+    });
+    document.getElementById("eda").addEventListener("click", function () {
+        document.body.classList.toggle("top-active");
+        document.body.classList.remove("left-active", "right-active");
+    });
+    document.getElementById("tech").addEventListener("click", function () {
+        document.body.classList.toggle("right-active");
+        document.body.classList.remove("left-active", "top-active");
+    });
+*/
+    var squares = document.getElementsByTagName("td");
+    for (var i=0;i<squares.length;i++) {
+        squares[i].addEventListener("click", makeMove);
+    }
+});
