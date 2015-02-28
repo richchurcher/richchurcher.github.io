a516f1ceee43932f918faac3f43712086de3b6f3
Rich Churcher <rich.churcher@gmail.com>
2015-03-01 12:47:57 +1300

    Styling. Remove comments. Add messages.

--- tictactoe.js
+++ tictactoe.js
@@ -134,19 +134,6 @@ var makeMove = function(e) {
 }
 
 document.addEventListener("DOMContentLoaded", function(event) { 
-/*    document.getElementById("tictactoe").addEventListener("click", function () {
-        document.body.classList.toggle("left-active");
-        document.body.classList.remove("top-active", "right-active");
-    });
-    document.getElementById("eda").addEventListener("click", function () {
-        document.body.classList.toggle("top-active");
-        document.body.classList.remove("left-active", "right-active");
-    });
-    document.getElementById("tech").addEventListener("click", function () {
-        document.body.classList.toggle("right-active");
-        document.body.classList.remove("left-active", "top-active");
-    });
-*/
     var squares = document.getElementsByClassName("square");
     for (var i=0;i<squares.length;i++) {
         squares[i].addEventListener("click", makeMove);
